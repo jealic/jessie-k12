9c6bf6b49476253a43444de5c24c97f277c831c6
Jessie Ling <jealic@gmail.com>
2018-10-06 20:10:27 +0800

    redirect root in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    @user = current_user
+    if signed_in?
+      redirect_to user_lessons_path(current_user)
+    else
+      redirect_to root_path
+    end
+  end
 end
