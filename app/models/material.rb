0dc797776bc9a79e8bac449429af1fdb2a52f62b
Jessie Ling <jealic@gmail.com>
2018-10-06 23:12:53 +0800

    set material and plan model

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,7 @@
 class Material < ApplicationRecord
   belongs_to :user
-  has_many :teachingfiles
+  belongs_to :subject_tag
+  has_many :teachingfiles, dependent: :destroy
+
+  validates_presence_of :m_name
 end
