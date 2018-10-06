59df81018572d4164ca562b975231ddfe1d45385
Jessie Ling <jealic@gmail.com>
2018-10-06 23:58:04 +0800

    allowed attachments stored as an array

--- app/controllers/teachingfiles_controller.rb
+++ app/controllers/teachingfiles_controller.rb
@@ -1,2 +1,10 @@
 class TeachingfilesController < ApplicationController
+
+
+
+  private
+
+  def teachingfile_params
+    params.require(:teachingfile).permit(:name,:material_id,:plan_id, :user_id, {attachments: []})
+  end
 end
