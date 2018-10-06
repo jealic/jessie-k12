484ab00f25ea7a882629dba5ab2620e826c09490
Jessie Ling <jealic@gmail.com>
2018-10-06 20:04:34 +0800

    fixed routes typo and add seed data

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :todos
 
     resources :plans do
-      reources :teachingfiles
+      resources :teachingfiles
     end
 
     resources :materials do
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
         resources :aims
       end
     end
+
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+User.create!(
+  name: "admin",
+  intro: "Hi, I'm Adimn!",
+  job_title: "K-12 Project Developer",
+  email: "admin@example.com",
+  password: "12345678",
+  role: "admin",
+  website: "www.k12-project.com",
+  share_class_count: [*5..30].sample
+  )
+puts "Default admin created!"
+####################################
+# Subject_tag
+SubjectTag.destroy_all
+subject_tag_list = [
+  { name: "國文" },
+  { name: "英文" },
+  { name: "數學" },
+  { name: "社會" },
+  { name: "自然" },
+  { name: "生活科技" },
+  { name: "藝術" },
+  { name: "音樂" },
+  { name: "體育" },
+  { name: "班級經營" },
+  { name: "家政" },
+  { name: "健康教育" },
+  { name: "其他" }
+]
+
+subject_tag_list.each do |subject|
+  SubjectTag.create!( name: subject[:name] )
+end
+puts "Subject_tag created!"
