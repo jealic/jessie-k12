e8125041714ee1a952f802d515671a62edfb5266
Jessie Ling <jealic@gmail.com>
2018-10-06 19:56:52 +0800

    set routes for present controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  resources :todos
+
+  root "users#index"
+
+  resources :users do
+
+    resources :todos
+
+    resources :plans do
+      reources :teachingfiles
+    end
+
+    resources :materials do
+      resources :teachingfiles
+    end
+
+    resources :lessons do
+      resources :topics do
+        resources :aims
+      end
+    end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
